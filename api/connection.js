19b2b3a6497b0220133369bf859b4b31d736f840
Htet Naing <htetnaing.chess@gmail.com>
2020-12-03 17:24:53 +0630

    commit

--- api/connection.js
+++ api/connection.js
@@ -2,19 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://Htet_Naing:expense@expense.mtdqk.mongodb.net/Expense?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true 
 })
 
-
-// const me = new User({
-//     userName: 'John',
-//     department: 'Design Team',
-//     role: 'Employee',
-//     password: '123'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
--- api/models/users.js
+++ api/models/users.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema(
+    {
     userName: {
         type: String
     },
@@ -12,5 +14,43 @@ const User = mongoose.model('User', {
     },
     password: {
         type: String
+    },
+    approver: {
+        type: Boolean
+    },
+    requestor: {
+        type: Boolean
     }
+}
+)
+
+userSchema.statics.findByCredentials = async (userName, password) => {
+    const user = await User.findOne({ userName })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+//Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
 })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
